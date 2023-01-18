d346cb6abfe9c7f49ca7a9675efe82fd07c78235
hippies67 <ikbal.malik26@gmail.com>
2023-01-19 00:53:27 +0700

    fix

--- resources/views/front/team/index.blade.php
+++ resources/views/front/team/index.blade.php
@@ -323,7 +323,7 @@ and (max-device-width : 283px)
     $.ajax({
         url: "{{ route('team.ref-divisi') }}",
         method: "POST",
-        headers: {'X-CSRF-TOKEN': {{ csrf_token() }}},
+        headers: {'X-CSRF-TOKEN': '{{ csrf_token() }}'},
         data: {
             _token: "{{ csrf_token() }}" 
         },
@@ -413,7 +413,7 @@ and (max-device-width : 283px)
         $.ajax({
             url: "{{ route('team.ref-divisi') }}",
             method: "POST",
-            headers: {'X-CSRF-TOKEN': {{ csrf_token() }}},
+            headers: {'X-CSRF-TOKEN': '{{ csrf_token() }}'},
             data: {
                 _token: "{{ csrf_token() }}",
                 ref_periode_id: ref_periode_id
